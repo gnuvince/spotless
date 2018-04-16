be5325031cef17941c6d68deea298610f6d8ef57
Vincent Foley <vfoley@gmail.com>
2018-04-16 10:33:17 -0400

    Change main function to continuously read from stdin

--- main.c
+++ main.c
@@ -9,22 +9,20 @@
 
 
 int main(void) {
-    char *buf = malloc(BUFSIZE);
-    if (!buf)
-        errx(1, "could not allocate buffer");
-    size_t read = fread(buf, sizeof(char), BUFSIZE, stdin);
-    if (read == 0)
-        errx(1, "could not read input");
-    if (!feof(stdin))
-        errx(1, "input is too long");
-
+    char buf[BUFSIZE];
     struct Scanner scanner;
-    scanner.stream = buf;
-    scanner.err_msg = "no value";
-    while (scanner.curr_token != TOK_EOF) {
-        if (next(&scanner) == RESULT_FAIL) {
-            errx(1, "%s", scanner.err_msg);
+
+    while (!feof(stdin)) {
+        fgets(buf, BUFSIZE, stdin);
+
+        scanner.stream = buf;
+        scanner.err_msg = "no value";
+        while (scanner.curr_token != TOK_EOF) {
+            if (next(&scanner) == RESULT_FAIL) {
+                errx(1, "%s", scanner.err_msg);
+            }
         }
     }
+
     return 0;
 }
